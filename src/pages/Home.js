3a43f92950601852b04f6c9dd12f8895d8454d28
Tymothy Sellers <tsellers1990@gmail.com>
2020-08-20 15:52:11 -0600

    added a home.js file

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render() {
+        return(
+            <div>
+                <div className="Title">
+                    <h1>
+                        This is the Title
+                    </h1>
+                </div>
+                <div className="Showcase">
+
+                </div>
+                <div className="Items">
+
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default Home;
